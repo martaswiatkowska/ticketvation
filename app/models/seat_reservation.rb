0cda1a224556ae33e09ce181622a4576d0e675a0
martaswiatkowska <martaswiatkowska8@gmail.com>
2021-02-17 13:44:26 +0100

    Add missing frozen string

--- app/models/seat_reservation.rb
+++ app/models/seat_reservation.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class SeatReservation < ApplicationRecord
   belongs_to :seat
   belongs_to :reservation
--- db/migrate/20210217115816_create_seat_reservations.rb
+++ db/migrate/20210217115816_create_seat_reservations.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateSeatReservations < ActiveRecord::Migration[6.0]
   def change
     create_table :seat_reservations do |t|
