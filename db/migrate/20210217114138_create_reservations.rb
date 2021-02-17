08ca67c66bef71a5dac18a9b4c175bfbcb7619b3
martaswiatkowska <martaswiatkowska8@gmail.com>
2021-02-17 12:44:31 +0100

    add Reservation model

---
+++ db/migrate/20210217114138_create_reservations.rb
@@ -0,0 +1,13 @@
+class CreateReservations < ActiveRecord::Migration[6.0]
+  def change
+    create_table :reservations do |t|
+      t.string :name
+      t.string :surname
+      t.string :email
+      t.string :status
+      t.belongs_to :event, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
