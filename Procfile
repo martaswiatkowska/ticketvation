e482ca6b6187e66f64cf0e2d6fe277e35b75667f
martaswiatkowska <martaswiatkowska8@gmail.com>
2021-02-18 15:38:13 +0100

    Add sidekiq and foreman

---
+++ Procfile
@@ -0,0 +1,3 @@
+web: bundle exec puma -p $PORT
+webpack: bin/webpack-dev-server
+job: bundle exec sidekiq -q reservations -q mailers
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
+# frozen_string_literal: true
+
+
 Rails.application.routes.draw do
+  require 'sidekiq/web'
   resources :events
   resources :reservations, only: [:index, :new, :create]
+  get "reservation/payment"
+  mount Sidekiq::Web => "/sidekiq"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
